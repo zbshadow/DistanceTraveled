46921c841509a62d2b0f411b9e59e1800000e8a8
zbshadow <zbshadowx@gmail.com>
2018-10-21 00:23:03 -0400

    initial commit

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+/*
+Program Name: Distance Traveled
+Author: B. Bayless
+Date last Updated: 10/27/2018
+Purpose: Calculate Distance Traaveled based on given speed and time.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//ask user for speed of a vehicle in mph
+
+	//how many hours it has travled
+
+	//show distance after each hour passed untill max 
+
+
+	return 0;
+}
