d57bb97a55834878878352447c44e3eea98a2246
zbshadow <zbshadowx@gmail.com>
2018-10-21 01:55:10 -0400

    I/O added

--- Source.cpp
+++ Source.cpp
@@ -10,12 +10,16 @@ Purpose: Calculate Distance Traaveled based on given speed and time.
 using namespace std;
 
 int main() {
+	int speed;
+	float hoursTraveled;
 	//ask user for speed of a vehicle in mph
+	cout << "What is the speed in mph?" << endl;
+	cin >> speed;
 
 	//how many hours it has travled
+	cout << "How many hours of travel?" << endl;
+	cin >> hoursTraveled;
 
 	//show distance after each hour passed untill max 
-
-
 	return 0;
 }
