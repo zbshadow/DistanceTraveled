dbea30a8e795ec6c2aa24f0212738d1e06689d2d
zbshadow <zbshadowx@gmail.com>
2018-10-21 02:40:34 -0400

    Loop to show each hours Distance

--- Source.cpp
+++ Source.cpp
@@ -6,20 +6,32 @@ Purpose: Calculate Distance Traaveled based on given speed and time.
 */
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
 	int speed;
+	int i = 0;
 	float hoursTraveled;
-	//ask user for speed of a vehicle in mph
 	cout << "What is the speed in mph?" << endl;
 	cin >> speed;
 
-	//how many hours it has travled
 	cout << "How many hours of travel?" << endl;
 	cin >> hoursTraveled;
 
-	//show distance after each hour passed untill max 
+	cout << "You entered " << speed << "mph and you drove for " << hoursTraveled << " hours." << endl;
+	cout << "Here is a break down of how far you traveled by hour." << endl;
+	cout << " --------------------" << endl;
+	cout << left;
+	cout << " " << setw(10) <<  "Hour"  << "Miles" << endl;
+	i = 1;
+
+	while ( i <= hoursTraveled) 
+	{
+		int dist = i * speed;
+		cout << " " << setw(10) << i << setw(10) << dist << endl;
+		i = i + 1;
+	}
 	return 0;
 }
